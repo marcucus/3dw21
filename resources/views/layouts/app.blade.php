84af02615eac6808cec299407bf73ee5d999220f
marcucus <marquesadrien.pro@gmail.com>
2022-04-08 01:29:53 +0000

    save

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,13 +17,14 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link href="../../css/app.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/membres') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('nouvellepage','App\Http\Controllers\MonControleur@retourneNouvellePage');
 Route::get('membres', 'App\Http\Controllers\ControleurMembres@index');
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\ControleurMembres@index');
 
 Route::get('/hello', function () {
     echo '<h1>Bonjour !</h1>';
